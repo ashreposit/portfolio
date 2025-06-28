0104ad95df673ab654971dc940de4068b2aa400b
ashreposit <anithaasha12@gmail.com>
2025-06-28 07:31:05 +0530

    Basic project structure with vite and tailwindcss

---
+++ asha-portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='bg-amber-200 bg-auto text-blue-700'>Hello world</p>
+    </>
+  )
+}
+
+export default App
